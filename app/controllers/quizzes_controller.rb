9df64d2e1baeea6c14a93267cbd451637753ddcd
Isay <yasnet21@gmail.com>
2019-05-24 23:12:32 -0500

    Save quiz plan in redis

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -2,23 +2,41 @@ class QuizzesController < ApplicationController
   before_action :set_category
 
   def show
-    service = CreateQuizPlan.new(@category)
+    service = CreateQuizPlan.new(@category, repository)
     plan = service.perform
-    word_id = plan.first.first
 
-    @strategy = plan.first.last
-    @word = service.words.find(word_id)
+    @strategy = plan.strategy_class_name
+    @word = plan.word
   end
 
   def update
-    @word = @category.words.includes(:wordable, translations: %i[wordable]).find(quiz_params[:id])
-    strategy = quiz_params[:strategy].safe_constantize.new(@word)
+    quiz_plan = QuizPlan.new(
+      repository.fetch_plan,
+      @category.words.includes(:wordable, translations: %i[wordable])
+    )
 
-    return redirect_to category_quiz_path(@category, id: @word.id) if strategy.valid?(quiz_params[:answer])
-    render :show
+    if quiz_plan.strategy.valid?(quiz_params[:answer])
+      quiz_plan.corrects << [quiz_plan.word.id, quiz_plan.strategy_class_name]
+    else
+      quiz_plan.mistakes << [quiz_plan.word.id, quiz_plan.strategy_class_name]
+    end
+
+    if quiz_plan.index + 1 == quiz_plan.size
+      # Redirect to result page
+      repository.delete_plan
+      return redirect_to category_path(@category)
+    end
+
+    repository.save_plan(plan: quiz_plan.plan_array, current: quiz_plan.index + 1, size: quiz_plan.size,
+                         mistakes: quiz_plan.mistakes, corrects: quiz_plan.corrects)
+    redirect_to category_quiz_path(@category)
   end
 
   private
+
+  def repository
+    RedisRepository.new("machetito-#{@category.id}")
+  end
 
   def set_category
     @category = Category.find(params[:category_id])
--- app/services/create_quiz_plan.rb
+++ app/services/create_quiz_plan.rb
@@ -8,15 +8,36 @@ class CreateQuizPlan
     'kanji' => Plans::KanjiPlan
   }.freeze
 
-  def initialize(category)
+  def initialize(category, repository)
     @category = category
+    @repository = repository
   end
 
   def perform
-    words.flat_map do |word|
+    QuizPlan.new(fetch_or_create_plan, words)
+  end
+
+  private
+
+  def create_plan
+    plan = words.flat_map do |word|
       plan = PLANS[word.wordable_type]
       plan&.perform(word)
     end.compact.shuffle
+
+    plan_attrs = { plan: plan, current: 0, size: plan.length, mistakes: [], corrects: [] }
+    @repository.save_plan(plan_attrs)
+    plan_attrs
+  end
+
+  def fetch_or_create_plan
+    fetch_plan.present? && fetch_plan || create_plan
+  end
+
+  def fetch_plan
+    @repository.fetch_plan
+  rescue StandardError => _e
+    nil
   end
 
   def words
