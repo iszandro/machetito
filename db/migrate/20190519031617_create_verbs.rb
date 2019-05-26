0d02847a7ed8f96787341b8cfc0fe30e9bbbb22a
Isay Sosa <yasnet21@gmail.com>
2019-05-25 22:06:35 -0500

    Fix schema and migration (#8)

--- db/migrate/20190519031617_create_verbs.rb
+++ db/migrate/20190519031617_create_verbs.rb
@@ -2,7 +2,7 @@ class CreateVerbs < ActiveRecord::Migration[5.2]
   def change
     create_table :verbs do |t|
       t.string :name
-      t.string :language
+      t.string :language, null: false
       t.jsonb :meta, default: {}
 
       t.timestamps
