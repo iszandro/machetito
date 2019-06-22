23b2cf0a80fe09b042200836ae20a5a3f72667cf
Isay <yasnet21@gmail.com>
2019-06-22 16:12:42 -0500

    Use `wanakana` from browser instead of npm module

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,16 @@
-import { bind } from 'wanakana';
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
 
-document.addEventListener("DOMContentLoaded", function() {
-  const hiraganaInput = document.querySelector('.wanakana');
-  const katakanaInput = document.querySelector('.wanakana-katakana');
 
-  hiraganaInput && bind(hiraganaInput);
-  katakanaInput && bind(katakanaInput, {IMEMode: 'toKatakana'});
-});
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
