5ef9c55bcad87677e3f598d4816382f47595d2a9
Isay <yasnet21@gmail.com>
2019-07-29 15:23:03 -0500

    Delay `sidekiq` when sending invitation email

--- app/services/send_invitation_email.rb
+++ app/services/send_invitation_email.rb
@@ -4,7 +4,11 @@ module SendInvitationEmail
   module_function
 
   def perform_later(user_id)
+    delay.perform(user_id)
+  end
+
+  def perform
     user = User.find(user_id)
-    UserMailer.with(email: user.email, invite_token: user.invite_token).invitation_email.deliver_later
+    UserMailer.with(email: user.email, invite_token: user.invite_token).invitation_email.deliver_now
   end
 end
